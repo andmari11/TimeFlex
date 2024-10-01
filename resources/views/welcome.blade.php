a61386c98e54df78cb60a186f14291866526f038
alexlopez2303 <alejlo24@ucm.es>
2024-10-01 18:38:56 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,7 @@
 <x-layout :title="'Bienvenido a la mejor aplicación web de planificación de horarios'">
-
+    <div class="flex flex-col items-center">
+        <p class="text-base font-normal text-gray-700">Somos la empresa líder en automatización de procesos de RRHH</p>
+        <p class="text-base font-normal text-gray-700">Los empleados nos eligen como la mejor empresa en cuanto a satisfacción de los usuarios</p>
+        <p class="text-base font-normal text-gray-700">Permitimos procesar los turnos y vacaciones de todos los empleados en menos de 5 minutos</p>
+    </div>
 </x-layout>
