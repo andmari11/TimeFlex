b80d1ce51ad6ea39eec03e63172035fd6c50e0d8
andmari11 <andres.mari.piqueras@gmail.com>
2024-10-02 15:43:50 +0200

    welcomeCambios

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -112,11 +112,6 @@
         </div>
     </nav>
 
-    <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{$title}}</h1>
-        </div>
-    </header>
     <main>
         {{$slot}}
     </main>
