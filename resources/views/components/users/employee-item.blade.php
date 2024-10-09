9f57a6d00e85a8cb3dcf6dbe7e0b13560a98e4a3
alvarm34 <alvarm34@ucm.es>
2024-10-09 14:46:29 +0200

    poner tailwind listacompañeros

--- resources/views/components/users/employee-item.blade.php
+++ resources/views/components/users/employee-item.blade.php
@@ -1,3 +1,27 @@
-<li>
-    <b>{{$employee->name}}:</b> {{$employee->role}}
-</li>
+<div>
+    <img class="h-8 w-8 rounded-full" src="https://static.vecteezy.com/system/resources/previews/004/274/186/non_2x/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg" alt="">
+
+</div>
+<div class="p-2 text-white text-bold text-l">
+    <h3>{{$employee->name}}</h3>
+</div>
+<div class=" flex justify-between">
+    <div>
+        <a href="#" class=" bg-white/10 hover:bg-white/35 px-2 py-1 rounded-xl text-xs text-white">{{$employee->role}}</a>
+    </div>
+    <div>
+        @if(auth()->user()->role === 'admin')
+            <button>
+                <a href="/shifts/{{$employee->id}}/edit">
+                    <img class="h-8 w-8" src="{{ asset('editar.png') }}" alt="editar">
+                </a>
+            </button>
+            <button>
+                <img class="h-8 w-8" src="{{ asset('elimina.png') }}" alt="eliminar">
+            </button>
+        @endif
+    </div>
+
+</div>
+
+
--- resources/views/mi-area.blade.php
+++ resources/views/mi-area.blade.php
@@ -1,24 +1,31 @@
 <x-layout :title="'Mi área'">
     <x-page-heading>Bienvenido a tu área personal</x-page-heading>
-    <div class="w-full max-w-md bg-white p-8 rounded-lg shadow-md mt-10">
-        @auth()
-            @if(auth()->user()->role === 'admin')
-                <p>Compañeros:</p>
-                <a href="register" class="bg-white text-blue-900 font-bold py-2 px-4 my-12 rounded-full border-2 border-blue-900 hover:bg-blue-900 hover:text-white transition relative top-6"> + </a>
-            @endif
-            @foreach(auth()->user()->company->employees as $employee)
-                <x-users.employee-item :employee="$employee"></x-users.employee-item>
-                @if(auth()->user()->role === 'admin')
-                <button>
-                    <a href="/shifts/{{$employee->id}}/edit">
-                        <img class="h-8 w-8" src="{{ asset('editar.png') }}" alt="editar">
-                    </a>
-                </button>
-                <button>
-                    <img class="h-8 w-8" src="{{ asset('elimina.png') }}" alt="eliminar">
-                </button>
-                @endif
-            @endforeach
-        @endauth
-    </div>
+    <section class="w-full max-w-md bg-white px-8 rounded-lg shadow-md mt-10">
+            <nav class="flex justify-between items-center py-5 border-b border-blue/10">
+                <div class="inline-flex items-center gap-x-2">
+
+                    <span class="w-2 h-2 bg-black inline-block"></span>
+                    <h3 class="text-bold text-xl">Compañeros</h3>
+                </div>
+                <div>
+                    @if(auth()->user()->role === 'admin')
+
+                        <a href="register" class="bg-white text-blue-900 font-bold py-2 px-4 my-12 rounded-full border-2 border-blue-900 hover:bg-blue-900 hover:text-white transition"> + </a>
+                    @endif
+                </div>
+
+            </nav>
+            <section class="p-4 rounded-xl flex flex-col text-center">
+
+                    @foreach(auth()->user()->company->employees as $employee)
+                    <div class="p-4 bg-gray-600 shadow rounded-xl my-1">
+                        <x-users.employee-item :employee="$employee"></x-users.employee-item>
+                    </div>
+                    @endforeach
+
+
+            </section>
+
+
+    </section>
 </x-layout>
