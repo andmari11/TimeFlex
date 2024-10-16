9f05f6bb6bf9d38e603161f598544019328bc1cc
alvarm34 <alvarm34@ucm.es>
2024-10-16 13:19:07 +0200

    cambio posicion botones

--- resources/views/components/users/employee-item.blade.php
+++ resources/views/components/users/employee-item.blade.php
@@ -1,13 +1,6 @@
-<div>
-    <img class="h-8 w-8 rounded-full" src="https://static.vecteezy.com/system/resources/previews/004/274/186/non_2x/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg" alt="">
-
-</div>
-<div class="p-2 text-white text-bold text-l">
-    <h3>{{$employee->name}}</h3>
-</div>
 <div class=" flex justify-between">
     <div>
-        <a href="#" class=" bg-white/10 hover:bg-white/35 px-2 py-1 rounded-xl text-xs text-white">{{$employee->role}}</a>
+        <img class="h-8 w-8 rounded-full" src="https://static.vecteezy.com/system/resources/previews/004/274/186/non_2x/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg" alt="">
     </div>
     <div>
         @if(auth()->user()->role === 'admin')
@@ -27,6 +20,15 @@
             </button>--}}
         @endif
     </div>
+
+</div>
+
+<div class="p-2 text-white text-bold text-l">
+    <h3>{{$employee->name}}</h3>
+</div>
+
+<div class="flex justify-start">
+    <a href="#" class=" bg-white/10 hover:bg-white/35 px-2 py-1 rounded-xl text-xs text-white">{{$employee->role}}</a>
 
 </div>
 
