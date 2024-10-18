1cb7f12aadd1139e98fdf997bcb2e44537cc6cdf
andmari11 <andres.mari.piqueras@gmail.com>
2024-10-18 17:00:24 +0200

    alert al eliminar usuario

--- resources/views/components/users/employee-item.blade.php
+++ resources/views/components/users/employee-item.blade.php
@@ -1,26 +1,17 @@
-<div class=" flex justify-between">
+<div class="flex justify-between">
     <div>
         <img class="h-8 w-8 rounded-full" src="https://static.vecteezy.com/system/resources/previews/004/274/186/non_2x/person-icon-user-interface-icon-silhouette-of-man-simple-symbol-a-glyph-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-ect-vector.jpg" alt="">
     </div>
     <div>
         @if(auth()->user()->role === 'admin')
             <a href="/users/{{$employee->id}}/edit" class="bg-blue-500 hover:bg-white/35 px-2 py-1 rounded-xl text-xs text-white">Editar</a>
-            <button form="delete-form-{{$employee->id}}"  class=" bg-red-600 hover:bg-white/35 px-2 py-1 rounded-xl text-xs text-white">Eliminar</button>
+            <button onclick="confirmDelete(event, {{$employee->id}})" class="bg-red-600 hover:bg-white/35 px-2 py-1 rounded-xl text-xs text-white">Eliminar</button>
             <form method="POST" action="/users/{{$employee->id}}/delete" id="delete-form-{{$employee->id}}" class="hidden">
                 @csrf
                 @method('DELETE')
             </form>
-            {{--<button>
-                <a href="/shifts/{{$employee->id}}/edit">
-                    <img class="h-8 w-8" src="{{ asset('editar.png') }}" alt="editar">
-                </a>
-            </button>
-            <button>
-                <img class="h-8 w-8" src="{{ asset('elimina.png') }}" alt="eliminar">
-            </button>--}}
         @endif
     </div>
-
 </div>
 
 <div class="p-2 text-white text-bold text-l">
@@ -28,8 +19,15 @@
 </div>
 
 <div class="flex justify-start">
-    <a href="#" class=" bg-white/10 hover:bg-white/35 px-2 py-1 rounded-xl text-xs text-white">{{$employee->section?->name}}</a>
-
+    <a href="#" class="bg-white/10 hover:bg-white/35 px-2 py-1 rounded-xl text-xs text-white">{{$employee->section?->name}}</a>
 </div>
 
-
+<script>
+    function confirmDelete(event, employeeId) {
+        event.preventDefault(); // Evita que se envíe el formulario inmediatamente
+        const confirmation = confirm("¿Estás seguro de que deseas eliminar este empleado?");
+        if (confirmation) {
+            document.getElementById('delete-form-' + employeeId).submit(); // Envía el formulario si el usuario confirma
+        }
+    }
+</script>
