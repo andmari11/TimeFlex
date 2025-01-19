7bd1dd5d5fdc2aff8ac4e4f6a9c37eda51dc5105
andmari11 <andres.mari.piqueras@gmail.com>
2025-01-19 12:49:33 +0100

    contacta y about us

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,30 +1,82 @@
 <x-layout :title="'Contacto'"></x-layout>
 
-<div class="bg-white py-24 sm:py-32">
-    <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-        <div class="max-w-2xl">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contáctanos para mejorar tu empresa</h2>
-            <p class="mt-6 text-lg leading-8 text-gray-600">Eficiencia, claridad y organización al alcance de un click.</p>
-            <p class="mt-6 text-lg leading-8 text-gray-600">¿Cómo contactar con nosotros?</p>
-            <p class="mt-6 text-lg leading-8 text-gray-600"> </p>
-            <ul role="list" class="space-y-6">
-                <li>
-                    <div class="flex items-center">
-                        <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Envíanos un mensaje por email a contacto@timeflex.es</h3>
-                    </div>
-                </li>
-                <li>
-                    <div class="flex items-center">
-                        <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Llama a nuestro teléfono de empresa: 694839221</h3>
-                    </div>
-                </li>
-                <li>
-                    <div class="flex items-center">
-                        <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Mándanos un mensaje a través de redes sociales</h3>
-                    </div>
-                </li>
-            </ul>
+<div class="bg-white">
+    <div class="isolate bg-white px-6 py-8 sm:py-20 lg:px-8">
+
+        <div class="mx-auto max-w-2xl text-center">
+            <h2 class="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Contáctanos</h2>
+            <p class="mt-2 text-lg/8 text-gray-600">¿Tienes preguntas o quieres probar TimeFlex? Escríbenos y descubre cómo podemos ayudarte.</p>
         </div>
-        <img width="100%" height="auto" src="https://images.pexels.com/photos/8866790/pexels-photo-8866790.jpeg?auto=compress&cs=tinysrgb&w=800" alt="">
+        <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div>
+                    <label for="first-name" class="block text-sm/6 font-semibold text-gray-900">Nombre</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                    </div>
+                </div>
+                <div>
+                    <label for="last-name" class="block text-sm/6 font-semibold text-gray-900">Apellidos</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                    </div>
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="company" class="block text-sm/6 font-semibold text-gray-900">Nombre de empresa</label>
+                    <div class="mt-2.5">
+                        <input type="text" name="company" id="company" autocomplete="organization" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                    </div>
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="email" class="block text-sm/6 font-semibold text-gray-900">Correo electrónico</label>
+                    <div class="mt-2.5">
+                        <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+                    </div>
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="phone-number" class="block text-sm/6 font-semibold text-gray-900">Número de teléfono</label>
+                    <div class="mt-2.5">
+                        <div class="flex rounded-md bg-white outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                            <div class="grid shrink-0 grid-cols-1 focus-within:relative">
+                                <select id="country" name="country" autocomplete="country" aria-label="Country" class="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pl-3.5 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                                    <option>ES</option>
+                                    <option>US</option>
+                                    <option>CA</option>
+                                    <option>EU</option>
+                                </select>
+                                <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
+                                    <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <input type="text" name="phone-number" id="phone-number" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="123 456 789">
+                        </div>
+                    </div>
+                </div>
+                <div class="sm:col-span-2">
+                    <label for="message" class="block text-sm/6 font-semibold text-gray-900">Mensaje</label>
+                    <div class="mt-2.5">
+                        <textarea name="message" id="message" rows="4" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
+                    </div>
+                </div>
+                <div class="flex gap-x-4 sm:col-span-2">
+                    <div class="flex h-6 items-center">
+                        <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
+                        <button type="button" class="flex w-8 flex-none cursor-pointer rounded-full bg-gray-200 p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" role="switch" aria-checked="false" aria-labelledby="switch-1-label">
+                            <span class="sr-only">Agree to policies</span>
+                            <!-- Enabled: "translate-x-3.5", Not Enabled: "translate-x-0" -->
+                            <span aria-hidden="true" class="size-4 translate-x-0 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"></span>
+                        </button>
+                    </div>
+                    <label class="text-sm/6 text-gray-600" id="switch-1-label">
+                        Confirme que ha leído y acepta nuestra
+                        <a href="#" class="font-semibold text-indigo-600">política&nbsp;de privacidad</a>.
+                    </label>
+                </div>
+            </div>
+            <div class="mt-10">
+                <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+            </div>
+        </form>
     </div>
+
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,56 +13,55 @@
                 <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
             </svg>
         </div>
-        <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+        <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-4 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
             <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div class="lg:pr-4">
                     <div class="lg:max-w-lg">
-                        <p class="text-base font-semibold leading-7 text-indigo-600">Tu opinión importa</p>
-                        <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Adapta tu trabajo a ti</h1>
-                        <p class="mt-6 text-xl leading-8 text-gray-700">TimeFlex es la única aplicación que te permite organizar de forma automatizada horarios y turnos de trabajo a la vez que maximiza la satisfacción de los trabajadores y facilita las labores de RRHH</p>
+                        <p class="text-base font-semibold leading-7 text-indigo-600">Olvídate del caos</p>
+                    </div>
+                    <div class="lg:max-w-xl">
+                        <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Haz que el trabajo se adapte a ti</h1>
+                    </div>
+                    <div class="lg:max-w-lg">
+                        <p class="mt-6 text-xl leading-8 text-gray-700">TimeFlex es la única solución que automatiza la gestión de turnos y horarios, aumentando la satisfacción de los empleados y simplificando el trabajo de RRHH.</p>
                     </div>
                 </div>
             </div>
             <div class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-                <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png" alt="">
+                <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="{{asset('mi-area.PNG')}}" alt="Interfaz de TimeFlex">
             </div>
             <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div class="lg:pr-4">
                     <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-                        <p>En un mundo de caos y desorden, TimeFlex sabe hacer todo más fácil que nunca. ¿Turnos que coinciden? ¿Preferencias que no son compatibles? Deja atrás los rompecabezas e invierte tu tiempo laboral en lo realmente importante, Timeflex solucionará todos esos inconvenientes por ti</p>
+                        <p>En un entorno laboral complejo, TimeFlex simplifica la planificación. ¿Turnos que se solapan? ¿Preferencias difíciles de cuadrar? Olvídate de los problemas y céntrate en lo realmente importante. Nosotros nos encargamos del resto.</p>
                         <ul role="list" class="mt-8 space-y-8 text-gray-600">
                             <li class="flex gap-x-3">
-                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
+                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                    <path d="M3 10h18v4H3v-4zm0 6h18v4H3v-4zm0-12h18v4H3V4z" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900">Elaboración automatizada de horarios</strong> Tan solo con hacer un click, TimeFlex elaborará un horario personalizado para las preferencias de tus trabajadores y las necesidades de tu empresa. Sí, así de fácil</span>
+                                <span><strong class="font-semibold text-gray-900">Horarios automatizados en un clic<br></strong> TimeFlex genera automáticamente turnos optimizados según las preferencias de los empleados y las necesidades de tu empresa.</span>
                             </li>
                             <li class="flex gap-x-3">
-                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                    <path d="M12 22c5.52 0 10-4.48 10-10S17.52 2 12 2 2 6.48 2 12s4.48 10 10 10zm-1-9h2v6h-2v-6zm0-4h2v2h-2V9z" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900">Satisfacción entre los empleados y mejora del ambiente laboral</strong> Timeflex tiene en cuenta las preferencias de cada empleado, y consigue satisfacer a todos los trabajadores mediante un sistema de compensación que convierte la asignación de turnos en justa y conveniente</span>
+                                <span><strong class="font-semibold text-gray-900">Mejor clima laboral <br></strong> Nuestro sistema de compensación equilibra los turnos para que todos los empleados trabajen con satisfacción y justicia.</span>
                             </li>
                             <li class="flex gap-x-3">
-                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
-                                    <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
+                                <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                    <path d="M9 16h6v-6h4l-7-7-7 7h4v6z" />
                                 </svg>
-                                <span><strong class="font-semibold text-gray-900">Trabajar en Recursos Humanos nunca había sido tan sencillo</strong> Gracias a TimeFlex, la pesadilla de cuadrar horarios dejará de existir entre los trabajadores de RRHH, y por ello, tus trabajadores podrán centrar sus esfuerzos en tareas que realmente aporten valor a tu organización</span>
+                                <span><strong class="font-semibold text-gray-900">Gestión de RRHH sin estrés<br></strong> TimeFlex elimina la dificultad de cuadrar horarios, permitiendo que RRHH se enfoque en tareas estratégicas.</span>
                             </li>
                         </ul>
-                        <p class="mt-8">Horarios automatizados, empleados felices y optimización del trabajo de áreas como RRHH. ¿Qué más se puede pedir? Tan solo Timeflex puede lograr todo eso de una sola tacada. No esperes más y convierte TimeFlex en uno de los activos clave de tu empresa</p>
-                        <p class="mt-8"> </p>
-                        <button >
-                            <a href="/contact" class="btn bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                                Contactar para adquirir TimeFlex
-                            </a>
-                        </button>
-                        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">¿Sigues sin decidirte? No hay problema</h2>
-                        <p class="mt-6">TimeFlex es un software pionero que mejorará de forma significativa el rendimiento de tu entidad, sin embargo, entendemos que un escenario tan bueno te cause desconfianza de primeras. Por ello, permitimos que las empresas interesadas en nuestro producto adquieran una demo gratuita de prueba de 15 días para probar las indudables bondades de TimeFlex. ¿A qué esperas para darnos una oportunidad? Ya no hay excusas. </p>
+
+                        <p class="mt-8">Horarios optimizados, empleados más felices y una gestión eficiente de RRHH. Todo en una sola plataforma. No esperes más, haz de TimeFlex un pilar clave en tu empresa.</p>
+                        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">¿Aún tienes dudas? ¡Pruébalo gratis!</h2>
+                        <p class="mt-6">Sabemos que es difícil de creer, por eso te ofrecemos una demo gratuita de 15 días. Descubre por ti mismo cómo TimeFlex revoluciona la gestión de horarios.</p>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </x-layout>
