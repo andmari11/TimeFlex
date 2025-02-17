c0762359d5aaa610588e77ceb149d9278796e7a3
andmari11 <andres.mari.piqueras@gmail.com>
2025-02-17 22:54:20 +0100

    arreglo error notificaciones

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -71,13 +71,8 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                                 </svg>
                                 <div id="notification-dot-container">
-                                    @php
-                                        $notifications = auth()->user()->unreadNotifications->count();
-                                    @endphp
 
-                                    @if($notifications > 0)
                                         <span id="notification-dot" class="absolute top-0 right-0 block h-2.5 w-2.5 rounded-full bg-red-500 ring-2 ring-gray-800"></span>
-                                    @endif
                                 </div>
                             </button>
 
