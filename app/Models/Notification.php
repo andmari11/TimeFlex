4723ad2c4222586cee8a857c11e1baa0b1e793a0
alexlopez2303 <alejlo24@ucm.es>
2025-03-27 00:35:38 +0100

    cambios notificaciones

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id', 'message', 'url', 'read', 'tipo'];
+    protected $fillable = ['user_id', 'message', 'url', 'read', 'tipo', 'email', 'nombre', 'apellidos', 'duda'];
     public function user()
     {
         return $this->belongsTo(User::class);
