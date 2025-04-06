5189656de64f1fe1a7b1a463977e96fb32577097
alvarm34 <alvarm34@ucm.es>
2025-04-06 17:15:22 +0200

    cambio diseños forms horarios

--- resources/views/schedules/edit.blade.php
+++ resources/views/schedules/edit.blade.php
@@ -1,44 +1,68 @@
 <x-layout :title="'Editar Horario'">
-    <div class="flex items-center justify-center">
-        <form method="POST" action="/horario/{{ $schedule->id }}/edit" enctype="multipart/form-data" class="w-full max-w-md bg-white p-8 rounded-lg shadow-md mt-10">
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 class="text-4xl text-center font-bold text-gray-800 mt-10 mb-5">Editar Horario</h1>
+        <form method="POST" action="/horario/{{ $schedule->id }}/edit" enctype="multipart/form-data"
+              class="bg-white border border-gray-200 rounded-lg shadow-lg p-8 max-w-4xl w-full mx-auto mt-10 mb-20">
             @csrf
             @method('PATCH')
             <div class="space-y-1 row">
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="name">Título del horario</x-forms.label>
-                    <x-forms.input name="name" id="name" :value="old('name', $schedule->name)" required />
+                    <label for="name" class="block text-lg font-medium text-gray-700">Título del horario</label>
+                    <input name="name" id="name" value="{{ old('name', $schedule->name) }}" required
+                           class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"/>
                     <x-forms.error name="name" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="description">Descripción</x-forms.label>
-                    <x-forms.input name="description" id="description" :value="old('description', $schedule->description)" required />
+                    <label for="description" class="block text-lg font-medium text-gray-700">Descripción</label>
+                    <input name="description" id="description" value="{{ old('description', $schedule->description) }}" required
+                           class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"/>
                     <x-forms.error name="description" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="section_id">Sección</x-forms.label>
-                    <select name="section_id" id="section_id" class="w-full rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md py-1.5 px-3" required>
+                    <label for="section_id" class="block text-lg font-medium text-gray-700">Sección</label>
+                    <select name="section_id" id="section_id"
+                            class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                            required>
                         @foreach($sections as $section)
-                            <option value="{{ $section->id }}" {{ $section->id == old('section_id', $schedule->section_id) ? 'selected' : '' }}>{{ $section->name }}</option>
+                            <option value="{{ $section->id }}" {{ $section->id == old('section_id', $schedule->section_id) ? 'selected' : '' }}>
+                                {{ $section->name }}
+                            </option>
                         @endforeach
                     </select>
                     <x-forms.error name="section_id" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="start_date">Fecha de inicio</x-forms.label>
-                    <input type="text" name="start_date" id="start_date" value="{{ $schedule->start_date}}" class="w-full  rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md py-1.5 px-3" required>
+                    <label for="start_date" class="block text-lg font-medium text-gray-700">Fecha de inicio</label>
+                    <input type="text" name="start_date" id="start_date" value="{{ $schedule->start_date }}"
+                           class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500" required>
                     <x-forms.error name="start_date" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="end_date">Fecha de fin</x-forms.label>
-                    <input type="text" name="end_date" id="end_date" value="{{ $schedule->end_date }}" class="w-full  rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md py-1.5 px-3" required>
+                    <label for="end_date" class="block text-lg font-medium text-gray-700">Fecha de fin</label>
+                    <input type="text" name="end_date" id="end_date" value="{{ $schedule->end_date }}"
+                           class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500" required>
                     <x-forms.error name="end_date" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <a href="/horario/{{$schedule->id}}/edit/shift-type/create" class="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Añadir turnos</a>
+                    <a href="/horario/{{$schedule->id}}/edit/shift-type/create"
+                       class="rounded-md bg-green-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
+                        Añadir turnos
+                    </a>
                 </x-forms.field>
+
             </div>
-            <div class="mt-6 flex items-center justify-between">
-                <a href="/horario" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cancelar</a>
+
+            <div class="mt-10 flex items-center justify-between">
+                <a href="/horario"
+                   class="rounded-md bg-red-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Cancelar
+                </a>
                 <x-forms.button>Actualizar horario</x-forms.button>
             </div>
         </form>
@@ -49,7 +73,6 @@
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             flatpickr("#start_date");
-
             flatpickr("#end_date");
         });
     </script>
--- resources/views/schedules/register.blade.php
+++ resources/views/schedules/register.blade.php
@@ -1,54 +1,69 @@
 <x-layout :title="'Registro'">
-    <div class=" flex items-center justify-center ">
-        <form method="POST" action="/horario-registrar" enctype="multipart/form-data" class="w-full max-w-md bg-white p-8 rounded-lg shadow-md mt-10">
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 class="text-4xl text-center font-bold text-gray-800 mt-10 mb-5">Crear Nuevo Horario</h1>
+        <form method="POST" action="/horario-registrar" enctype="multipart/form-data"
+              class="bg-white border border-gray-200 rounded-lg shadow-lg p-8 max-w-4xl w-full mx-auto mt-10 mb-20">
             @csrf
-            <div class="space-y-1 row" >
 
+            <div class="space-y-1 row">
 
                 <x-forms.field class="col-12">
-                    <x-forms.label for="name">Título del horario</x-forms.label>
-                    <x-forms.input name="name" id="name" :value="old('name')" required />
+                    <label for="name" class="block text-lg font-medium text-gray-700">Título del horario</label>
+                    <input name="name" id="name" value="{{ old('name') }}" required
+                           class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"/>
                     <x-forms.error name="name" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="description">Descripción</x-forms.label>
-                    <textarea class="w-full flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md py-1.5 px-3" name="description" id="description" required></textarea>
-                    <x-forms.error name="decription" />
+                    <label for="description" class="block text-lg font-medium text-gray-700">Descripción</label>
+                    <textarea class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                              name="description" id="description" required></textarea>
+                    <x-forms.error name="description" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="section_id">Sección</x-forms.label>
-                    <select name="section_id" id="section_id" class="w-full flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md py-1.5 px-3" required>
+                    <label for="section_id" class="block text-lg font-medium text-gray-700">Sección</label>
+                    <select name="section_id" id="section_id"
+                            class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                            required>
                         @foreach($sections as $section)
                             <option value="{{ $section->id }}">{{ $section->name }}</option>
                         @endforeach
                     </select>
                     <x-forms.error name="section_id" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="start_date">Fecha de inicio</x-forms.label>
-                    <input type="text" name="start_date" id="start_date" value="{{ old('start_date') }}" class="w-full flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md py-1.5 px-3" required>
+                    <label for="start_date" class="block text-lg font-medium text-gray-700">Fecha de inicio</label>
+                    <input type="text" name="start_date" id="start_date" value="{{ old('start_date') }}"
+                           class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500" required>
                     <x-forms.error name="start_date" />
                 </x-forms.field>
+
                 <x-forms.field class="col-12">
-                    <x-forms.label for="end_date">Fecha de fin</x-forms.label>
-                    <input type="text" name="end_date" id="end_date" value="{{ old('end_date') }}" class="w-full flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md py-1.5 px-3" required>
+                    <label for="end_date" class="block text-lg font-medium text-gray-700">Fecha de fin</label>
+                    <input type="text" name="end_date" id="end_date" value="{{ old('end_date') }}"
+                           class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500" required>
                     <x-forms.error name="end_date" />
                 </x-forms.field>
 
             </div>
 
-            <div class="mt-6 flex items-center justify-between">
-                <a href="/menu" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'">Cancelar</a>
+            <div class="mt-10 flex items-center justify-between">
+                <a href="/menu"
+                   class="rounded-md bg-red-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Cancelar
+                </a>
                 <x-forms.button>Registrar horario</x-forms.button>
             </div>
         </form>
     </div>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             flatpickr("#start_date");
-
             flatpickr("#end_date");
         });
     </script>
