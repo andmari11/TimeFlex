8e8182b0324d1b47f2a28797f7189f286baf1f62
alexlopez2303 <alejlo24@ucm.es>
2025-04-11 15:07:40 +0200

    cambio en el seeder para meter intercambio de turnos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -198,5 +198,30 @@ class DatabaseSeeder extends Seeder
                 }
             });
         });
+
+        //metemos los ids de users y shifts en arrays
+        $users_ids = User::pluck('id')->toArray();
+        $shifts_ids = Shift::pluck('id')->toArray();
+
+        // creamos 60 estados y los mezclamos aleatoriamente
+        $statusarray = array_merge(
+            array_fill(0, 30, 'Accepted'),
+            array_fill(0, 20, 'Pending'),
+            array_fill(0, 10, 'Declined')
+        );
+        shuffle($statusarray);
+
+        foreach (range(1, 60) as $i) {
+            DB::table('shift_exchanges')->insert([
+                'demander_id' => $demander = fake()->randomElement($users_ids),
+                'receiver_id' => $receiver = fake()->randomElement(array_diff($users_ids, [$demander])),
+                'shift_demander_id' => fake()->randomElement($shifts_ids),
+                'shift_receiver_id' => fake()->randomElement($shifts_ids),
+                'reason' => '',
+                'status' => $statusarray[$i - 1],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
