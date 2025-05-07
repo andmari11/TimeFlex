89a9d79851979fe1a9da14fc520e446732f510b5
andmari11 <andres.mari.piqueras@gmail.com>
2025-05-07 21:35:51 +0200

    seeder user no lo subi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Company;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -48,5 +49,65 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public static function createMultiples()
+    {
+        User::factory(1)->create([
+            'name' => 'Usuario',
+            'email' => "user@user.com",
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'company_id' => 1,
+            'role' => 'employee',
+            'section_id' => 0
+        ]);
+        User::factory()->create([
+            'name' => 'Administrador',
+            'email' => "admin@admin.com",
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'company_id' => 1,
+            'role' => 'admin',
+            'weight' => 5,
+            'section_id' => 1
+        ]);
+
+        $secciones = [
+            2 => [
+                'Juan Pérez', 'Marta Ruiz', 'Sergio Navarro', 'Laura Ortega', 'Pedro Molina',
+                'Sandra Ramos', 'David Ibáñez', 'Raquel Vidal', 'Antonio Peña', 'Nuria Lozano'
+            ],
+            3 => [
+                'Jorge Cano', 'Beatriz Herrera', 'Rubén Morales', 'Cristina León', 'Iván Salas',
+                'Patricia Gil', 'Óscar Méndez', 'Eva Castro', 'Andrés Ríos', 'Clara Domínguez'
+            ],
+            4 => [
+                'Manuel Serrano', 'Elena Varela', 'Adrián Romero', 'Rosa Campos', 'Diego Pascual',
+                'Lucía Cordero', 'Alberto Pastor', 'Carla Ferrer', 'Tomás Nieto', 'Irene Benítez'
+            ],
+            5 => [
+                'Álvaro Aguado', 'Celia Marín', 'Hugo Esteban', 'Aitana Valle', 'Enrique Cortés',
+                'Sofía Blanco', 'Pablo Lobo', 'Lidia Bermejo', 'Daniel Soler', 'Noelia Saez'
+            ]
+        ];
+
+        foreach ($secciones as $sectionId => $nombres) {
+            foreach ($nombres as $index => $nombre) {
+
+                User::factory()->create([
+                    'name' => $nombre,
+                    'email' => 'usuario' . $sectionId .$index . '@hospital.es',
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('password'),
+                    'remember_token' => Str::random(10),
+                    'company_id' => 1,
+                    'role' => 'employee',
+                    'section_id' => $sectionId
+                ]);
+            }
+        }
     }
 }
